717d3de39e5fe23254a722be503a216909653c33
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-01-18 11:54:01 -0600

    Changed font and added favicon

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,15 +6,28 @@ const pageLoad = function() {
     let header = document.createElement("header");
     let title = document.createElement("div");
     title.id = "title";
+
     let nav = document.createElement("div");
     nav.id = "nav";
-    nav.textContent = "menu";
+
+    let navHome = document.createElement("div");
+    navHome.textContent = "Home";
+    navHome.classList.add("nav-list");
+
+    let navMenu = document.createElement("div");
+    navMenu.textContent = "Menu";
+    navMenu.classList.add("nav-list");
+
+    let navContact = document.createElement("div");
+    navContact.textContent = "Contact Us";
+    navContact.classList.add("nav-list");
+
+    nav.append(navHome, navMenu, navContact);
+
+
     title.textContent = "Le Café de l’Espérance";
     content.appendChild(header);
     header.append(title, nav);
-
-
-
 
 }
 
