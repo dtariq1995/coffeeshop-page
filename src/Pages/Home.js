e93c4ecf71bee08e47babe4573833f2be565b0ad
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-01-18 10:02:44 -0600

    Added background and skeleton for header

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,21 @@
 const pageLoad = function() {
 
+
     let content = document.querySelector("#content");
 
-    let heading = document.createElement("h1");
-    heading.id = "title";
-    heading.textContent = "Restaurant Name";
-    content.appendChild(heading);
+    let header = document.createElement("header");
+    let title = document.createElement("div");
+    title.id = "title";
+    let nav = document.createElement("div");
+    nav.id = "nav";
+    nav.textContent = "menu";
+    title.textContent = "Le Café de l’Espérance";
+    content.appendChild(header);
+    header.append(title, nav);
+
+
+
+
 }
 
 export default pageLoad;
