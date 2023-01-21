e7f6f94978f4452612ebfc3c9751e952334da953
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-01-21 12:57:10 -0600

    Finished footer design

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ const pageLoad = function() {
 
     let content = document.querySelector("#content");
 
-    let header = document.createElement("header");
+    let header = document.createElement("header");  // header content
     let title = document.createElement("div");
     title.id = "title";
 
@@ -21,14 +21,38 @@ const pageLoad = function() {
     let navContact = document.createElement("div");
     navContact.textContent = "Contact Us";
     navContact.classList.add("nav-list");
-
     nav.append(navHome, navMenu, navContact);
 
-
-    title.textContent = "Le Café de l’Espérance";
+    title.textContent = "Café l’Espérance";
     content.appendChild(header);
     header.append(title, nav);
 
+
+    let homeContent = document.createElement("div");  // home content
+    homeContent.id = "home";
+    content.appendChild(homeContent);
+
+    let homeText = document.createElement("div");
+    homeText.classList.add("homeText");
+    homeText.textContent = "Experience the world's greatest French pressed coffee";``
+
+    homeContent.append(homeText);
+
+
+
+
+    let footer = document.createElement("footer");  // footer content
+    let footerText = document.createElement("p");
+    let footerLink = document.createElement("a");
+    let footerImg = document.createElement("img");
+
+    footerText.textContent = "Created by:";
+    footerImg.src = "/src/Assets/GitHub-Mark-Light-32px.png";
+    footerLink.href = "https://github.com/dtariq1995";
+    footerLink.textContent = "Daanyaal Tariq";
+
+    content.appendChild(footer);
+    footer.append(footerText, footerImg, footerLink);
 }
 
 export default pageLoad;
