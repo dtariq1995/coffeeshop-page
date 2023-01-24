ee9447ded839eb34210d3410dca4ec35918627d5
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-01-24 16:43:38 -0600

    Finished homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -34,9 +34,21 @@ const pageLoad = function() {
 
     let homeText = document.createElement("div");
     homeText.classList.add("homeText");
-    homeText.textContent = "Experience the world's greatest French pressed coffee";``
+    homeText.textContent = "Experience France's Greatest Coffee";
 
-    homeContent.append(homeText);
+    let homeTextTwo = document.createElement("div");
+    homeTextTwo.classList.add("homeText");
+    homeTextTwo.textContent = "Serving Delicious Coffee Since 1995";
+
+    let homeImg = document.createElement("img");
+    homeImg.src = "/src/Assets/coffee-profile2.jpg";
+
+    let homeTextThree = document.createElement("div");
+    homeTextThree.classList.add("homeText");
+    homeTextThree.textContent = "Order Online Or Visit Us Today";
+
+
+    homeContent.append(homeText, homeTextTwo, homeImg, homeTextThree);
 
 
 
